fb82084701f1c0b0c0cba55435665c97ebe5eb06
Jake Anderson <jacob.colin.anderson@icloud.com>
2020-06-22 14:56:19 -0400

    added time_stamp

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import requests
 from decimal import *
+from datetime import datetime
 
 def lambda_handler(event, context):
     
@@ -15,8 +16,11 @@ def lambda_handler(event, context):
 
     req = requests.get(f'http://ip-api.com/json/{ip_address}')
     location_data = req.json()
+
+    now = datetime.now()
+    time_stamp = now.strftime("%m/%d/%Y %H:%M:%S")
     
-    table.put_item(Item={'IpAddress': ip_address, 'City': location_data["city"], 'Region': location_data["regionName"],"Lat": Decimal(str(location_data["lat"])), "Lon": Decimal(str(location_data["lon"])),})
+    table.put_item(Item={'IpAddress': ip_address, 'City': location_data["city"], 'Region': location_data["regionName"],"Lat": Decimal(str(location_data["lat"])), "Lon": Decimal(str(location_data["lon"])), "Time": time_stamp})
     
     return {
         "statusCode": 200,
